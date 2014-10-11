c8d69814dcd8b146f6fb57e166755198f1724493
Robin Kåveland Hansen <rkh@knowit.no>
2014-10-11 13:44:36 +0200

    Test InstancesOf.

---
+++ src/test/java/com/github/kaaveland/tryagain/InstancesOfTest.java
@@ -0,0 +1,22 @@
+package com.github.kaaveland.tryagain;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class InstancesOfTest {
+    ExceptionMatcher runtimeException = new InstancesOf(RuntimeException.class);
+
+    @Test
+    public void that_illegal_argument_exception_matches_instances_of_runtime_exception() {
+        assertThat(runtimeException.matches(new IllegalArgumentException()), is(true));
+    }
+
+    @Test
+    public void that_ioexception_does_not_match_instancesof_runtime_exception() {
+        assertThat(runtimeException.matches(new IOException()), is(false));
+    }
+}
