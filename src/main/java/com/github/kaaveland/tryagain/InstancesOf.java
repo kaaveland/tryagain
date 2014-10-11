dbf344a2c86e75e3c56418de0942727496ad8bf2
Robin Kåveland Hansen <rkh@knowit.no>
2014-10-11 13:40:53 +0200

    Add InstancesOf to match exceptions on instance.
    This differs from ExceptionIn on that it accepts subclasses, not only
    exact matching on classes.

---
+++ src/main/java/com/github/kaaveland/tryagain/InstancesOf.java
@@ -0,0 +1,29 @@
+package com.github.kaaveland.tryagain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class InstancesOf implements ExceptionMatcher {
+    private final List<Class<? extends Exception>> exceptionTypes;
+
+    public InstancesOf(final List<Class<? extends Exception>> exceptionTypes) {
+        this.exceptionTypes = Collections.unmodifiableList(exceptionTypes);
+    }
+
+    @SafeVarargs
+    public InstancesOf(Class<? extends Exception>... exceptionTypes) {
+        this(Arrays.asList(exceptionTypes));
+    }
+
+    @Override
+    public boolean matches(final Exception exception) {
+        for (Class<? extends Exception> exceptionType : exceptionTypes) {
+            if (exceptionType.isInstance(exception)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
