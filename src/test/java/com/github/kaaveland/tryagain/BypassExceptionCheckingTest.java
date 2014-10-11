d2362b8bb71d14d1bbe60876f18daa4c25978fce
Robin Kåveland Hansen <rkh@knowit.no>
2014-10-11 13:48:32 +0200

    Test BypassExceptionChecking for Retriable.

--- src/test/java/com/github/kaaveland/tryagain/BypassExceptionCheckingTest.java
+++ src/test/java/com/github/kaaveland/tryagain/BypassExceptionCheckingTest.java
@@ -13,6 +13,13 @@ public class BypassExceptionCheckingTest {
         }
     }
 
+    private class ThrowIOExceptionWithResult implements Retriable<String> {
+        @Override
+        public String execute(final int attempt) throws Exception {
+            throw new IOException();
+        }
+    }
+
     private Retrier retrier = Retrier.retryOn(new ExceptionMatcher() {
         @Override
         public boolean matches(final Exception exception) {
@@ -26,4 +33,9 @@ public class BypassExceptionCheckingTest {
         bypassExceptionChecking.execute(new ThrowIOException());
     }
 
+    @Test(expected = IOException.class)
+    public void that_bypass_exception_checking_allows_us_to_throw_checked_exceptions_unchecked_with_result() {
+        BypassExceptionChecking bypassExceptionChecking = new BypassExceptionChecking(retrier);
+        bypassExceptionChecking.execute(new ThrowIOExceptionWithResult());
+    }
 }
