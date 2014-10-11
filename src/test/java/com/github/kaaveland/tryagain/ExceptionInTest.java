2aeef332802cdaadcf37008092c17c8942c0dff2
Robin Kåveland Hansen <rkh@knowit.no>
2014-10-11 13:40:44 +0200

    Test ExceptionIn.

---
+++ src/test/java/com/github/kaaveland/tryagain/ExceptionInTest.java
@@ -0,0 +1,24 @@
+package com.github.kaaveland.tryagain;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ExceptionInTest {
+    private final ExceptionMatcher runtimeAndIOException = new ExceptionIn(RuntimeException.class, IOException.class);
+
+    @Test
+    public void that_exception_in_matches_given_exceptions() {
+        assertThat(runtimeAndIOException.matches(new RuntimeException()), is(true));
+        assertThat(runtimeAndIOException.matches(new IOException()), is(false));
+    }
+
+    @Test
+    public void that_exception_in_matches_exactly_and_not_subclasses() {
+        assertThat(runtimeAndIOException.matches(new IllegalArgumentException()), is(false));
+    }
+
+}
